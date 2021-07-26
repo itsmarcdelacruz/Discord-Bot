f96a704166b54cc3fe8ed302d3f54bc2f6213825
itsmarcdelacruz <itsmarcdelacruz@users.noreply.github.com>
2021-07-25 23:19:41 -0400

    Starting code

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+import random
+
+TOKEN = ''
+
+client = discord.Client()
+
+@client.event
+async def on_ready();
+    print('We have logged in as {0.user]'.format(client))
+
+client.run(TOKEN)
