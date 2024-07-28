dd381293035ac3fe0f62123c6cc22e0a624ba4ca
Marc Delacruz <55219683+itsmarcdelacruz@users.noreply.github.com>
2024-07-27 23:48:35 -0400

    Restarting and recoding bot
    Restarting and recoding bot

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 import discord
-import random
-import os
-from discord.ext import commands
 
-TOKEN = ''
+intents = discord.Intents.default()
+intents.message_content = True
 
-intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix = '?', intents = intents)
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity = discord.Game('May is gay!'))
-    print('Bot is online!')
+    print(f'We have logged in as {client.user}')
 
-@client.command()
-async def clear(ctx, amount = 5):
-    await ctx.channel.purge(limit = amount)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
-client.run(TOKEN)
+client.run('your token here')
